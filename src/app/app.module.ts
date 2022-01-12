06f203ecac5ed5e58661c702dc6c3fe84bee958f
Titouan <titouan.fuchs@uha.fr>
2022-01-12 10:08:53 +0100

    youhou

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrameIntroComponent } from './components/frames/frame-intro/frame-intro.component';
+import { MainButtonComponent } from './components/ui/main-button/main-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FrameIntroComponent
+    FrameIntroComponent,
+    MainButtonComponent
   ],
   imports: [
     BrowserModule,
