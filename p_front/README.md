85dea17da59ed8de05baa2cbc12ef5e67ab68f90
Titouan <titouan.fuchs@uha.fr>
2022-02-03 21:10:35 +0100

    new start

--- p_front/README.md
+++ p_front/README.md
@@ -1,4 +1,4 @@
-# Portfolio
+# PFront
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.1.2.
 
--- p_front/src/app/app.component.spec.ts
+++ p_front/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Portfolio'`, () => {
+  it(`should have as title 'p_front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Portfolio');
+    expect(app.title).toEqual('p_front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Portfolio app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('p_front app is running!');
   });
 });
