3634fa605cb5baef91b656f0a64048a239116270
Titouan <titouan.fuchs@uha.fr>
2022-02-09 06:25:47 +0100

    ajout du footer

--- p_front/src/app/app.module.ts
+++ p_front/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HomeViewComponent } from './views/home-view/home-view.component';
 import { ProjectsViewComponent } from './views/projects-view/projects-view.component';
+import { AboutViewComponent } from './views/about-view/about-view.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     HomeViewComponent,
-    ProjectsViewComponent
+    ProjectsViewComponent,
+    AboutViewComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
