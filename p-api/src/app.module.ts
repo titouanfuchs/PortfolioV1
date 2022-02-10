126714630e98323fdb3168fad96b192d243cf926
Titouan <titouan.fuchs@uha.fr>
2022-02-10 14:39:51 +0100

    [UPDATE] ajout lien BDD dans le back

--- p-api/src/app.module.ts
+++ p-api/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type:'mysql',
+    host:'portfolio-BDD',
+    port:+process.env.MYSQL_PORT,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    entities: ["dist/**/*.entity{.ts,.js}"],
+    autoLoadEntities: true,
+    synchronize: process.env.TARGET === "dev"
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+
+  private constructor() {
+  }
+}
